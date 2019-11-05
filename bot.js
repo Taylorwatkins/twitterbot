c47d7b623a92ad2a883b9e4316b2add0f74a99d1
taylor <twatkins35@gatech.edu>
2019-11-05 12:52:14 -0500

    lowercase g

--- bot.js
+++ bot.js
@@ -72,8 +72,41 @@ function retweetWithoutT() {
 	});
 }
 
+var ugaSearch = {q: "#UGA", count: 10, result_type: "recent"}; 
+
+// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
+function retweetWithLittleG() {
+	T.get('search/tweets', ugaSearch, function (error, data) {
+	  // log out any errors and responses
+	  console.log(error, data);
+	  // If our search request to the server had no errors...
+	  if (!error) {
+	  	// ...then we grab the ID of the tweet we want to retweet...
+		var retweetId = data.statuses[0].text;
+        retweetId = retweetId.replace(/G/g, 'g');
+        var thwg = "THWg"
+        //retweetId = retweetId.replace(/t/g, '');
+		// ...and then we tell Twitter we want to retweet it!
+		T.post('statuses/update', {status: retweetId + "\nTHWg" }, function (error, response) {
+			if (response) {
+				console.log('Success! Check your bot, it should have retweeted something.')
+			}
+			// If there was an error with our Twitter call, we print it out here.
+			if (error) {
+				console.log('There was an error with Twitter:', error);
+			}
+		})
+	  }
+	  // However, if our original search request had an error, we want to print it out here.
+	  else {
+	  	console.log('There was an error with your hashtag search:', error);
+	  }
+	});
+}
+
 // Try to retweet something as soon as we run the program...
 retweetWithoutT();
+retweetWithLittleG();
 // ...and then every hour after that. Time here is in milliseconds, so
 // 1000 ms = 1 second, 1 sec * 60 = 1 min, 1 min * 60 = 1 hour --> 1000 * 60 * 60
 setInterval(retweetWithoutT, 1000 * 60 * 5);
